fb4568a89774f852b372acbe64cd0864c424ba2e
lcd344 <lcd344@yahoo.com>
2021-06-04 19:04:27 -0300

    tailwind instalado

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss'); /* Add this line at the top */
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -13,5 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        postCss: [tailwindcss('./tailwind.config.js')],
+    })
+    .version();
 
